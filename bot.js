ff7b37140e226f7255289cce85b59f31278a06c1
DinoWw <dinoplecko@yahoo.com>
2022-07-04 14:13:18 +0200

    changed command word to gm

--- bot.js
+++ bot.js
@@ -22,13 +22,13 @@ const clientDiscord = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]}
 clientDiscord.on("ready", () => {
 	console.log(`Logged in as ${clientDiscord.user.tag}!`) })
 	clientDiscord.on("message", async msg => {
-		if (msg.content === "ping") {
+		if (msg.content === "gm") {
 			
 			const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 			
 		  const userFeed = ig.feed.user(loggedInUser.pk);
 		  const myPostsFirstPage = await userFeed.items();
-			console.log(myPostsFirstPage[0]);
+			//console.log(myPostsFirstPage[0]);
 			msg.reply(`https://www.instagram.com/p/${myPostsFirstPage[0].code}`);
 			
 	}
